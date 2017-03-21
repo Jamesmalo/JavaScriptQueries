932848dfabdd9c379c63cf695b28bd04e9832940
Jamesmalo <9jmalo@stpatrick.org>
2017-03-21 10:52:12 -0500

    day 1 work

---
+++ click.js
@@ -0,0 +1,13 @@
+//declare samecolor() here
+function samecolor(hex){
+  const doc = document;
+  let divs = doc.querySelectorAll("#samecolor div")
+    for(let i=0;i<samecolor.length;i++){
+      samecolor[i].style.background-color = ;
+    }
+
+}
+function diffcolor(hex){
+
+}
+//declare diffcolor() here
