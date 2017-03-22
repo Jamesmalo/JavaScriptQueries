2d3267160689ba66f8d401cfa2dabcc55e90012e
Jamesmalo <9jmalo@stpatrick.org>
2017-03-22 11:26:33 -0500

    day 2

--- click.js
+++ click.js
@@ -1,13 +1,18 @@
 //declare samecolor() here
-function samecolor(hex){
+function samecolor(){
+  let color = "#009900"
   const doc = document;
-  let divs = doc.querySelectorAll("#samecolor div")
-    for(let i=0;i<samecolor.length;i++){
-      samecolor[i].style.background-color = ;
+  let divs = doc.querySelectorAll("#samecolor div");
+    for(let i=0;i<divs.length;i++){
+      divs[i].style.backgroundColor = color;
     }
-
 }
-function diffcolor(hex){
-
+function diffcolor(){
+  let color = "#990099"
+  const doc = document;
+  let divs = doc.querySelectorAll("#diffcolor div")
+    for(let i=0;i<samecolor.length;i++){
+      divs[i].style.backgroundColor = color;
+    }
 }
 //declare diffcolor() here
