67f81da51e8d3bb234ae46f566da075c64da304b
Jamesmalo <9jmalo@stpatrick.org>
2017-03-23 10:48:59 -0500

    day 3

--- click.js
+++ click.js
@@ -5,14 +5,21 @@ function samecolor(){
   let divs = doc.querySelectorAll("#samecolor div");
     for(let i=0;i<divs.length;i++){
       divs[i].style.backgroundColor = color;
+      divs[i].style.opacity = Math.random();
     }
 }
 function diffcolor(){
   let color = "#990099"
+  let opcolor = "#66FF66"
   const doc = document;
   let divs = doc.querySelectorAll("#diffcolor div")
-    for(let i=0;i<samecolor.length;i++){
-      divs[i].style.backgroundColor = color;
+    for(let i=0;i<divs.length;i++){
+      if(i % 2 ==0){
+        return divs[i].style.backgroundColor = color;
+      }
+      else{
+        return divs[i].style.backgroundColor = opcolor;
+      }
     }
 }
 //declare diffcolor() here
--- resize.js
+++ resize.js
@@ -1,4 +1,9 @@
 //Declare tabletSize() here
+function tabletSize(){
 
+}
 
 //Declare phoneSize() here
+function phoneSize(){
+  
+}
